e72b501a26e44529abfd5dbe53da36bdf0475610
bdjones2410 <brandon.jones@blackbaud.com>
2017-07-27 19:06:13 -0400

    added wait spinner, and changed to radio icons

--- src/app/shared/form-submit.service.ts
+++ src/app/shared/form-submit.service.ts
@@ -5,12 +5,7 @@ import { Http } from '@angular/http';
 export class FormSubmitService {
   private URL: string = 'https://squaresfeedbackapi.azurewebsites.net/api/feedback';
   public constructor(private http: Http) { }
-  public submitData(formData: any): void {
-    this.http.post(this.URL, formData)
-      .subscribe(res => {
-        if (res.status === 200) {
-          // add stuff in here to close widget and thank for feedback.
-        }
-      });
+  public submitData(formData: any): any {
+    return this.http.post(this.URL, formData);
   }
 }
