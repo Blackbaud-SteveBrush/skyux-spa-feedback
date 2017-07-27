498b9ce1e774209a080ba29a355c0fb9551c09da
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-07-27 19:47:20 -0400

    Added event listeners

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor() {
+    window.addEventListener('message', (event) => {
+      if (event.origin !== 'https://host.nxt.blackbaud.com') {
+        return;
+      }
+
+      if (event.data.source === 'feedback-parent') {
+        console.log('Message received from template!', event.data.message);
+      }
+    }, false);
+
+    window.parent.postMessage({
+      message: 'Hello, template!',
+      source: 'feedback'
+    }, '*');
+  }
+}
